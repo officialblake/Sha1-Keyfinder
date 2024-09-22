c05fe2e4467cbbbc6b4f8da9ec6077e455b26ceb
Blake Marshall <github@blakemarshall.com>
2024-09-22 13:59:45 -0700

    add beta version

---
+++ main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"os/exec"
+)
+
+func main() {
+	var wordPath string
+	var tag string
+	var messagePath string
+	fmt.Print("Path to word bank: ")
+	fmt.Scan(&wordPath)
+
+	fmt.Print("Path to message: ")
+	fmt.Scan(&messagePath)
+
+	fmt.Print("Enter desried SHA1 tag: ")
+	fmt.Scan(&tag)
+
+	file, err := os.Open(wordPath)
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	defer file.Close()
+
+	_, err = os.Open(messagePath)
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		line := scanner.Text() // Get the text of the current line
+		// Run a simple terminal command, like "ls" (or "dir" for Windows)
+		cmd := exec.Command("openssl", "-dgst", "-sha1", "-mac", "-HMAC", "-macopt", "-hexkey:"+line, messagePath)
+		// Capture the output
+		output, err := cmd.Output()
+		if err != nil {
+			fmt.Println("Error running command:", err)
+			return
+		}
+		if string(output) == tag {
+			fmt.Println("Key found:", line)
+		}
+
+		// Print the output
+		fmt.Println(string(output))
+	}
+
+	// Check for errors that occurred during scanning
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error reading file:", err)
+	}
+
+}
